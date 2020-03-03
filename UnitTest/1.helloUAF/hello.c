7677a2ec44bbd60b5198396d526c7f89b0f68599
hikame <hikame@163.com>
2020-03-03 10:32:45 +0800

    modify unit test

--- UnitTest/1.helloUAF/hello.c
+++ UnitTest/1.helloUAF/hello.c
@@ -1,4 +1,15 @@
-#include "foo.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void foo(int argc) {
+    char* buf = malloc(10);
+    if(buf == NULL)
+        return;
+    buf[0] = 100;
+    free(buf);
+    if(buf != NULL)
+        buf[0];  // sink here
+}
 
 int main(int argc, char** argv) {
     foo(argc);
--- UnitTest/10.dynamic_GEP_Array/test.c
+++ UnitTest/10.dynamic_GEP_Array/test.c
@@ -1,7 +1,22 @@
-#include "stdlib.h"
+#include <stdlib.h>
 #include  <stdio.h>
-#include <string.h>
-#include <stdbool.h>
+
+void foo2(int arg) {
+  char* bf = malloc(10);
+  if(bf == NULL)
+    return;
+  char* array[10];
+  array[0] = bf;
+  free(array[arg]);
+  if(bf)
+    free(bf);  // sink, maybe false positive
+  return;
+}
+
+int main(int argc, char** argv) {
+  foo2(argc + 1);
+}
+
 
 void foo0(int arg) {
   char* bf = malloc(10);
@@ -27,28 +42,16 @@ void foo1(int arg) {
   return;
 }
 
-void foo2(int arg) {
-  char* bf = malloc(10);
-  if(bf == NULL)
-    return;
-  char* array[10];
-  array[0] = bf;
-  
-  free(array[arg]);
-  free(bf);  // sink, maybe false positive
-  return;
-}
-
 char teststr(const char *file){
     return file[0];
 }
 
-int main(int argc, char** argv) {
-  if(argc)
-    foo0(argc + 1);
-  else if(argc + 1)
-    foo1(argc + 1);
-  else
-    foo2(argc + 1);
-  teststr("abc");
-}
+// int main(int argc, char** argv) {
+//   if(argc)
+//     foo0(argc + 1);
+//   else if(argc + 1)
+//     foo1(argc + 1);
+//   else
+//     foo2(argc + 1);
+//   teststr("abc");
+// }
